865cc93cd7ab674d684c9a2d2682ddff032513d5
Liz Swabey-Keith <lizswabey-keith@Lizs-MacBook-Pro-2.local>
2016-12-07 10:29:20 -0600

    making progress

---
+++ server/app.js
@@ -0,0 +1,39 @@
+// var admin = require("firebase-admin");
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+var decoder = require('./modules/decoder');
+var privateData = require('./routes/private-data');
+var pgConnection = require('./modules/pg-connection');
+var mygames = require('./routes/mygames');
+var friendsgames = require('./routes/friendsgames');
+var about = require('./routes/about');
+var port = 3000;
+
+app.set("port", (process.env.PORT || port));
+
+app.get('/home', function(req, res) {
+    res.send("hello from the server");
+});
+
+app.use(express.static(path.resolve('./public')));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/views/index.html'));
+});
+
+app.use(decoder.token);
+
+app.use('/mygames', mygames);
+app.use('/friendsgames', friendsgames);
+app.use('/privateData', privateData);
+
+
+
+pgConnection.connect();
+
+app.listen(app.get("port"), function(){
+    console.log("Server up and running on Port", port);
+});
---
+++ server/modules/decoder.js
@@ -0,0 +1,22 @@
+var admin = require("firebase-admin");
+
+admin.initializeApp({
+  credential: admin.credential.cert("./server/firebase-service-account.json"),
+  databaseURL: "https://ytbnmtgfpsa.firebaseio.com"
+});
+
+var tokenDecoder = function(req, res, next){
+  admin.auth().verifyIdToken(req.headers.id_token).then(function(decodedToken) {
+    // Adding the decodedToken to the request so that downstream processes can use it
+    req.decodedToken = decodedToken;
+    next();
+  })
+  .catch(function(error) {
+    // If the id_token isn't right, you end up in this callback function
+    // Here we are returning a forbidden error
+    console.log('User token could not be verified');
+    res.send(403);
+  });
+}
+
+module.exports = { token: tokenDecoder };
